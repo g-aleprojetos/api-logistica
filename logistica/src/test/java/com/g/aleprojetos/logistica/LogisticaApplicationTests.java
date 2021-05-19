8444439c308d48174970745d8b7db9e7d776b57c
g-aleprojetos <g.aleprojetos@gmail.com>
2021-05-18 22:41:20 -0300

    create project "Logistica"

---
+++ logistica/src/test/java/com/g/aleprojetos/logistica/LogisticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g.aleprojetos.logistica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
