0f51b3c7ba7def03823f2803f7e12be81344460e
g-aleprojetos <g.aleprojetos@gmail.com>
2021-05-20 18:55:33 -0300

    Module 2 - continue

--- logistica/src/main/java/com/g/aleprojetos/logistica/domain/model/Cliente.java
+++ logistica/src/main/java/com/g/aleprojetos/logistica/domain/model/Cliente.java
@@ -5,6 +5,9 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Getter
@@ -17,10 +20,17 @@ public class Cliente {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank//impede que passei os dados null ou vazio
+    @Size(max = 60)//máximo de 60 caracteres
     private String nome;
 
+    @NotBlank//impede que passei os dados null ou vazio
+    @Email
+    @Size(max = 255)//máximo de 255 caracteres
     private String email;
 
+    @NotBlank//impede que passei os dados null ou vazio
+    @Size(max = 20)//máximo de 20 caracteres
     @Column(name = "fone")
     private String telefone;
 
