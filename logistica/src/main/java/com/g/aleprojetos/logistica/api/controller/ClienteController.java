1c61baaa0e07c44d4c2bcc5e626c9bb048e01a78
g-aleprojetos <g.aleprojetos@gmail.com>
2021-05-19 22:09:55 -0300

    Usando Jakarta Persistence

--- logistica/src/main/java/com/g/aleprojetos/logistica/api/controller/ClienteController.java
+++ logistica/src/main/java/com/g/aleprojetos/logistica/api/controller/ClienteController.java
@@ -4,27 +4,22 @@ import com.g.aleprojetos.logistica.domain.model.Cliente;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class ClienteController {
 
+    @PersistenceContext
+    private EntityManager manager;
+
     @GetMapping("/clientes")
     public List<Cliente> listar(){
 
-        var cliente1 = new Cliente();
-        cliente1.setId(1L);
-        cliente1.setNome("João");
-        cliente1.setEmail("joaodascouves@logistica.com");
-        cliente1.setTelefone("34 9999-1111");
+        return manager.createQuery("from Cliente", Cliente.class)
+                .getResultList();
 
-        var cliente2 = new Cliente();
-        cliente2.setId(2L);
-        cliente2.setNome("Maria");
-        cliente2.setEmail("mariadasilva@logistica.com");
-        cliente2.setTelefone("11 9777-2222");
-
-        return Arrays.asList(cliente1,cliente2);
     }
 }
